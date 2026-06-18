1597893338045ae1a29a58486b7dc2e352db8151
fzacariascallao <fzacariascallao@frba.utn.edu.ar>
2026-06-17 23:48:17 -0300

    Remuevo la entidad Alerta, su mapper y corrijo AlertService
    En un principio había pensado en persisitr las alertas, pero me di
    cuenta de que la consigna no lo pide así que lo saco ya que no las
    estaba usando.

--- src/main/java/ar/edu/utn/frba/dsi/climalert/service/AlertService.java
+++ src/main/java/ar/edu/utn/frba/dsi/climalert/service/AlertService.java
@@ -1,8 +1,6 @@
 package ar.edu.utn.frba.dsi.climalert.service;
 
-import ar.edu.utn.frba.dsi.climalert.domain.Alert;
 import ar.edu.utn.frba.dsi.climalert.domain.WeatherData;
-import ar.edu.utn.frba.dsi.climalert.mapper.AlertMapper;
 import ar.edu.utn.frba.dsi.climalert.repository.WeatherDataRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +8,17 @@ import org.springframework.stereotype.Service;
 public class AlertService {
   private final WeatherDataRepository weatherDataRepository;
   private final NotificationService notificationService;
-  private final AlertMapper alertMapper;
 
-  public AlertService(WeatherDataRepository weatherRepo, NotificationService notificationService, AlertMapper alertMapper) {
+  public AlertService(WeatherDataRepository weatherRepo, NotificationService notificationService) {
     this.weatherDataRepository = weatherRepo;
     this.notificationService = notificationService;
-    this.alertMapper = alertMapper;
   }
 
   public void checkAndAlert() {
     WeatherData ultimo = weatherDataRepository.findLatest();
     if (ultimo == null) return;
     if (ultimo.getTemperatura() > 35 && ultimo.getHumedad() > 60) {
-      Alert alerta = alertMapper.toAlert(ultimo);
       notificationService.sendAlert(ultimo);
-      alerta.setEnviado(true);
     }
   }
 }
